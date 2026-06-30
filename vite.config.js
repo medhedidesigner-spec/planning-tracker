c4e084b190d8b6d3ac5199eaabeb383ee13ef2a8
elhedihaddar <medhedi.designer@gmail.com>
2026-06-30 11:03:31 +0200

    fix: base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // IMPORTANT : remplace 'roadmap-app' par le nom exact de ton repo GitHub
-  base: '/roadmap-app/',
+  base: '/planning-tracker/',
 })
